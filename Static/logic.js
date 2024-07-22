51d45bbef359ad16ef8b21b1229d0b079ec34e2e
HGShelton <hannahgradyshelton@gmail.com>
2024-07-22 19:19:40 -0400

    Add cluster groups.

--- Static/logic.js
+++ Static/logic.js
@@ -1,7 +1,7 @@
 //Define a map object
 let myMap = L.map("map", {
     center: [42.3601, -71.0589],
-    zoom: 10
+    zoom: 13
 });
 
 // Add a tile layer
@@ -9,12 +9,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(myMap);
 
+// Create a MarkerClusterGroup
+let markers = L.markerClusterGroup();
+
 // Call data
 d3.json("listings.json").then(function (data) {
     console.log("Data loaded:", data);
 
     data.forEach(function (listing) {
-        console.log("Latitude:", listing.latitiude, "Longitude:", listing.longitude);
-        L.marker([listing.latitude, listing.longitude]).addTo(myMap);
+        console.log("Latitude:", listing.latitude, "Longitude:", listing.longitude);
+        let marker = L.marker([listing.latitude, listing.longitude]);
+        markers.addLayer(marker);
     });
+
+    myMap.addLayer(markers);
 });
