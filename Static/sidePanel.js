38dece04aa158e718c50942af9e14e9e913ad97e
HGShelton <hannahgradyshelton@gmail.com>
2024-07-28 21:33:13 -0400

    Add chart image

--- Static/sidePanel.js
+++ Static/sidePanel.js
@@ -1,145 +1,154 @@
-/**
- * @name Leaflet.SidePanel
- * @link https://github.com/maxwell-ilai/Leaflet.SidePanel
- */
-
 L.Control.SidePanel = L.Control.extend({
-	includes: L.Evented.prototype,
+    includes: L.Evented.prototype,
 
-	options: {
-		panelPosition: 'left',
-		hasTabs: true,
-		tabsPosition: 'top',
-		darkMode: false,
-		pushControls: false,
-		startTab: 1
-	},
+    options: {
+        panelPosition: 'left',  // Only left is supported
+        hasTabs: true,
+        tabsPosition: 'top',    // Keep tabs at the top for simplicity
+        darkMode: false,
+        pushControls: false,
+        startTab: 1
+    },
 
-	initialize: function (id, options) {
-		// Get panel HTMLElement
-		this._panel = L.DomUtil.get(id);
+    initialize: function (id, options) {
+        this._panel = L.DomUtil.get(id);
+        if (!this._panel) {
+            console.error('Side panel element not found.');
+        }
 
-		// Merge options
-		L.setOptions(this, options);
-	},
+        L.setOptions(this, options);
+    },
 
-	addTo: function (map) {
-		// Attach panel direction class
-		L.DomUtil.addClass(this._panel, 'sidepanel-' + this.options.panelPosition);
+    addTo: function (map) {
+        if (!this._panel) return;
 
-		// Enable dark mode
-		if (this.options.darkMode) {
-			L.DomUtil.addClass(this._panel, 'sidepanel-dark');
-		}
+        L.DomUtil.addClass(this._panel, 'sidepanel-' + this.options.panelPosition);
 
-		// Adds stopPropagation to the element's 'wheel' events (plus browser variants).
-		L.DomEvent.disableScrollPropagation(this._panel);
-		// Adds stopPropagation to the element's 'click', 'dblclick', 'contextmenu', 'mousedown' and 'touchstart' events (plus browser variants).
-		L.DomEvent.disableClickPropagation(this._panel);
+        if (this.options.darkMode) {
+            L.DomUtil.addClass(this._panel, 'sidepanel-dark');
+        }
 
-		// If there are tabs, start them.
-		if (this.options.hasTabs) {
-			this.initTabs(map, this.options.tabsPosition);
-		}
-	},
+        L.DomEvent.disableScrollPropagation(this._panel);
+        L.DomEvent.disableClickPropagation(this._panel);
 
-	initTabs: function (map, tabsPosition) {
-		if (typeof tabsPosition === 'string') {
-			L.DomUtil.addClass(this._panel, 'tabs-' + tabsPosition);
-		}
+        if (this.options.hasTabs) {
+            this.initTabs(map, this.options.tabsPosition);
+        }
+    },
 
-		let tabsLinks = this._panel.querySelectorAll('a.sidebar-tab-link');
-		let tabsContents = this._panel.querySelectorAll('.sidepanel-tab-content');
+    initTabs: function (map, tabsPosition) {
+        if (!this._panel) return;
 
-		tabsLinks.forEach(function (tabLink, tabIndex) {
-			// Shows the first active tab
-			let startTab, startContent;
+        if (typeof tabsPosition === 'string') {
+            L.DomUtil.addClass(this._panel, 'tabs-' + tabsPosition);
+        }
 
-			if (typeof this.options.startTab === 'number' && (this.options.startTab - 1) === tabIndex) {
-				startTab = tabLink;
-				startContent = tabsContents[tabIndex];
-			}
+        let tabsLinks = this._panel.querySelectorAll('a.sidebar-tab-link');
+        let tabsContents = this._panel.querySelectorAll('.sidepanel-tab-content');
 
-			if (typeof this.options.startTab === 'string' && this.options.startTab === tabLink.dataset.tabLink) {
-				startTab = tabLink;
-				startContent = this._panel.querySelector(`.sidepanel-tab-content[data-tab-content="${this.options.startTab}"]`);
-			}
+        tabsLinks.forEach((tabLink, tabIndex) => {
+            let startTab, startContent;
 
-			if (startTab !== undefined && !L.DomUtil.hasClass(startTab, 'active')) {
-				L.DomUtil.addClass(startTab, 'active');
-				L.DomUtil.addClass(startContent, 'active');
-			}
+            if (typeof this.options.startTab === 'number' && (this.options.startTab - 1) === tabIndex) {
+                startTab = tabLink;
+                startContent = tabsContents[tabIndex];
+            }
 
-			L.DomEvent.on(tabLink, 'click', function (e) {
-				L.DomEvent.preventDefault(e);
+            if (typeof this.options.startTab === 'string' && this.options.startTab === tabLink.dataset.tabLink) {
+                startTab = tabLink;
+                startContent = this._panel.querySelector(`.sidepanel-tab-content[data-tab-content="${this.options.startTab}"]`);
+            }
 
-				if (!L.DomUtil.hasClass(tabLink, 'active')) {
-					// Remove active links
-					for (let i = 0; i < tabsLinks.length; i++) {
-						let linkActive = tabsLinks[i];
+            if (startTab && !L.DomUtil.hasClass(startTab, 'active')) {
+                L.DomUtil.addClass(startTab, 'active');
+                L.DomUtil.addClass(startContent, 'active');
+            }
 
-						if (L.DomUtil.hasClass(linkActive, 'active')) {
-							L.DomUtil.removeClass(linkActive, 'active');
-						}
-					}
+            // Add image to tab
+            let img = L.DomUtil.create('img', 'tab-image', tabLink);
+            img.src = 'path/to/your/image.png';  // Path to your image
+            img.alt = 'Tab Image';
 
-					// Add current active link
-					L.DomUtil.addClass(tabLink, 'active');
+            L.DomEvent.on(tabLink, 'click', (e) => {
+                L.DomEvent.preventDefault(e);
 
-					// Shows current active content
-					tabsContents.forEach(function (element) {
-						if (tabLink.dataset.tabLink === element.dataset.tabContent) {
-							L.DomUtil.addClass(element, 'active');
-						} else {
-							L.DomUtil.removeClass(element, 'active');
-						}
-					});
-				}
-			}, tabLink);
-		}.bind(this));
+                if (!L.DomUtil.hasClass(tabLink, 'active')) {
+                    tabsLinks.forEach((linkActive) => {
+                        if (L.DomUtil.hasClass(linkActive, 'active')) {
+                            L.DomUtil.removeClass(linkActive, 'active');
+                        }
+                    });
 
-		this._toggleButton(map);
-	},
+                    L.DomUtil.addClass(tabLink, 'active');
 
-	_toggleButton: function (map) {
-		const container = this._panel.querySelector('.sidepanel-toggle-container');
-		const button = container.querySelector('.sidepanel-toggle-button');
+                    tabsContents.forEach((element) => {
+                        if (tabLink.dataset.tabLink === element.dataset.tabContent) {
+                            L.DomUtil.addClass(element, 'active');
+                        } else {
+                            L.DomUtil.removeClass(element, 'active');
+                        }
+                    });
+                }
+            });
+        });
 
-		L.DomEvent.on(button, 'click', function (e) {
-			let IS_OPENED = true;
-			let opened = L.DomUtil.hasClass(this._panel, 'opened');
-			let closed = L.DomUtil.hasClass(this._panel, 'closed');
+        this._toggleButton(map);
+    },
 
-			if (!opened && !closed) {
-				L.DomUtil.addClass(this._panel, 'opened');
-			} else if (!opened && closed) {
-				L.DomUtil.addClass(this._panel, 'opened');
-				L.DomUtil.removeClass(this._panel, 'closed');
-			} else if (opened && !closed) {
-				IS_OPENED = false;
-				L.DomUtil.removeClass(this._panel, 'opened');
-				L.DomUtil.addClass(this._panel, 'closed');
-			} else {
-				L.DomUtil.addClass(this._panel, 'opened');
-			}
+    _toggleButton: function (map) {
+        if (!this._panel) return;
 
-			if (this.options.pushControls) {
-				let controlsContainer = map.getContainer().querySelector('.leaflet-control-container');
+        const container = this._panel.querySelector('.sidepanel-toggle-container');
+        if (!container) {
+            console.error('Toggle container not found in the side panel.');
+            return;
+        }
 
-				L.DomUtil.addClass(controlsContainer, 'leaflet-anim-control-container');
+        const button = container.querySelector('.sidepanel-toggle-button');
+        if (!button) {
+            console.error('Toggle button not found in the toggle container.');
+            return;
+        }
 
-				if (IS_OPENED) {
-					L.DomUtil.removeClass(controlsContainer, this.options.panelPosition + '-closed');
-					L.DomUtil.addClass(controlsContainer, this.options.panelPosition + '-opened');
-				} else {
-					L.DomUtil.removeClass(controlsContainer, this.options.panelPosition + '-opened');
-					L.DomUtil.addClass(controlsContainer, this.options.panelPosition + '-closed');
-				}
-			}
-		}.bind(this), container);
-	},
+        L.DomEvent.on(button, 'click', (e) => {
+            let IS_OPENED = true;
+            let opened = L.DomUtil.hasClass(this._panel, 'opened');
+            let closed = L.DomUtil.hasClass(this._panel, 'closed');
+
+            if (!opened && !closed) {
+                L.DomUtil.addClass(this._panel, 'opened');
+            } else if (!opened && closed) {
+                L.DomUtil.addClass(this._panel, 'opened');
+                L.DomUtil.removeClass(this._panel, 'closed');
+            } else if (opened && !closed) {
+                IS_OPENED = false;
+                L.DomUtil.removeClass(this._panel, 'opened');
+                L.DomUtil.addClass(this._panel, 'closed');
+            } else {
+                L.DomUtil.addClass(this._panel, 'opened');
+            }
+
+            if (this.options.pushControls) {
+                let controlsContainer = map.getContainer().querySelector('.leaflet-control-container');
+                if (!controlsContainer) {
+                    console.error('Leaflet control container not found.');
+                    return;
+                }
+
+                L.DomUtil.addClass(controlsContainer, 'leaflet-anim-control-container');
+
+                if (IS_OPENED) {
+                    L.DomUtil.removeClass(controlsContainer, 'left-closed');
+                    L.DomUtil.addClass(controlsContainer, 'left-opened');
+                } else {
+                    L.DomUtil.removeClass(controlsContainer, 'left-opened');
+                    L.DomUtil.addClass(controlsContainer, 'left-closed');
+                }
+            }
+        });
+    },
 });
 
 L.control.sidepanel = function (id, options) {
-	return new L.Control.SidePanel(id, options);
+    return new L.Control.SidePanel(id, options);
 };
